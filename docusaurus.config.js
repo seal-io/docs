a476ea65ff0639e94a1f9f6051c63a6414ccbde8
gitlawr <lawrleegle@gmail.com>
2023-11-24 16:10:04 +0800

    feat: add resource & resource definition

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,7 +11,7 @@ const config = {
   tagline: 'Walrus Documentation',
   url: 'https://seal-io.github.io',
   baseUrl: '/docs/',
-  onBrokenLinks: 'throw',
+  onBrokenLinks: 'warn',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
 
